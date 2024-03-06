16a9ffe619bf57970a62def59a0bb829fe323540
manas <kaiymovmanas05@gmail.com>
2024-03-07 00:52:10 +0600

    save lab,py file in github

---
+++ lab.py
@@ -0,0 +1,62 @@
+
+# x = int(input('1 до 9: '))
+#
+# if x > 1 and x < 3:
+#     s = input('input string: ')
+#     n = int(input('input integer: '))
+#     for i in range(n):
+#         print(i, s)
+#
+# elif x > 4 and x < 6:
+#     m = int(input('input integer: '))
+#     print(x**m)
+#
+# elif x > 7 and x < 9:
+#     for i in range(10):
+#         x += 1
+#         print(x)
+#
+# else:
+#     print('Error')
+#
+
+# print('Общество в начале XXI века')
+# x = int(input('input ur y.o.'))
+#
+# if x > 0 and x < 7:
+#     print('Вам в детский сад')
+#
+# elif x > 7 and x < 18:
+#     print('Вам в проф учеб учреждение')
+#
+# elif x > 18 and x < 25:
+#     print('Вам на работу')
+#
+# elif x > 60 and x < 120:
+#     print('Вам предоставляется выбор')
+#
+# else:
+#     print('Error! Its only for human')
+
+
+# x = input('Введите Строку: ').split()
+# print(max(x, key=len))
+
+# x = input('Введите Строку: ').split(';')
+# print(max(x, key=len))
+
+# x = input('Введите Строку: ')
+# a = input('Введите разделитель: ')
+# print(min(x.split(a), key=len))
+
+# x = input('Найти слово: ')
+# w = input('Введите сктроку: ').split()
+# for i in w:
+#     if i == x:
+#         print('+', i)
+#     else:
+#         print('-', i)
+
+# x = input('Введите предложение: \n').split()
+# print(len(x), '- слов')
+
